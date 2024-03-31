7a35ef4295c3de48f0c292a9cc5317b73442c39f
zoetyande <zoetyandemohamed@yahoo.com>
2024-03-31 18:03:15 +0530

    Love Calculator using js

---
+++ LoveCalculator/index.js
@@ -0,0 +1,10 @@
+// Random NUmber generator
+
+// prompt("What is your name? ");
+// prompt('What is your boyfriend or girlfriend name? ');
+
+let r = Math.random();
+
+r = math.floor(r * 100) + 1;
+
+alert("The chances are: " + r + "%");
